2e8619ff6ed5326efce83f6133b06485a658516e
Mart7anova <mart7anova@gmail.com>
2023-07-12 20:01:14 +0300

    6.Code splitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import module from "./App.module.scss";
+import {Link, Route, Routes} from "react-router-dom";
+import {AboutPageLazy} from "./pages/AboutPage/AboutPage.lazy";
+import {MainPageLazy} from "./pages/MainPage/MainPage.lazy";
 
 export const App = () => {
     return (
-        <div className={module.title}>Helloooo</div>
+        <div className={module.title}>
+            <Link to='/'>Главная</Link>
+            <Link to='/about'>О сайте</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path='/' element={<MainPageLazy/>}/>
+                    <Route path='/about' element={<AboutPageLazy/>}/>
+                </Routes>
+            </Suspense>
+        </div>
     );
 };
