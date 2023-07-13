69802433f6004a12295a1513afe928d81d8bbc7b
Mart7anova <mart7anova@gmail.com>
2023-07-13 15:47:17 +0300

    7.add theme & theme context, theme hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import React, { Suspense } from 'react';
-import module from "./App.module.scss";
+import React, {Suspense} from 'react';
 import {Link, Route, Routes} from "react-router-dom";
 import {AboutPageLazy} from "./pages/AboutPage/AboutPage.lazy";
 import {MainPageLazy} from "./pages/MainPage/MainPage.lazy";
+import './styles/index.scss'
+import {useTheme} from "./theme/useTheme";
 
 export const App = () => {
+    const {theme, toggleTheme} = useTheme()
+
     return (
-        <div className={module.title}>
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>toggle</button>
             <Link to='/'>Главная</Link>
             <Link to='/about'>О сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
